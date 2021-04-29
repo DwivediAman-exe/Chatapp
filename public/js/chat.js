1e0a9fe8053bff6282124ebc0b86f8f8b3269e88
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-30 02:11:32 +0530

    location added

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,20 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 	e.preventDefault()
 
 	const message = e.target.elements.message.value
-
 	socket.emit('sendMessage',message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if(!navigator.geolocation) {
+		return alert('Geolocation is not supported in your browser')
+	}
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		const locationCoordinates = {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		}
+
+		socket.emit('sendLocation', (locationCoordinates))
+	})
 })
--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ io.on('connection', (socket) => {
 		io.emit('message', message)
 	})
 
+	socket.on('sendLocation', (location) => {
+		io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
+	})
+
 	socket.on('disconnect', () => {
 		io.emit('message', 'A user has left the chat :(')
 	})
