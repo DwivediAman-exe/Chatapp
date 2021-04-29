c636536394b839ecdbceb38b509403af39b5aa7e
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-30 00:50:39 +0530

    testing realtime message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+	console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const message = e.target.elements.message.value
+
+	socket.emit('sendMessage',message)
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const { Socket } = require('dgram')
 
 
 var app = express()
@@ -10,19 +11,21 @@ const server = http.createServer(app)
 // calling socketio function and configuring a given server
 const io = socketio(server)
 
+// setting up of port
+const port = process.env.PORT || 3000
 // for using and displaying static files
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
-
-
-
-// setting up of port
-const port = process.env.PORT || 3000
-
-io.on('connection', () => {
+io.on('connection', (socket) => {
 	console.log('New websocket connection added')
+
+	socket.emit('message', 'welcome')
+
+	socket.on('sendMessage', (message) => {
+		io.emit('message', message)
+	})
 })
 
 server.listen(port, () => {
