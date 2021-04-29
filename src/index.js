aea255537873b474358d0dfd6c87894780983f3c
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-29 19:39:56 +0530

    websocket setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
-var app = express();
+
+var app = express()
+// creating a new webserver
+const server = http.createServer(app)   
+// calling socketio function and configuring a given server
+const io = socketio(server)
+
+// for using and displaying static files
 const publicDirectoryPath = path.join(__dirname, '../public')
-
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req,res) => {
-	res.render('index');
-})
 
+
+
+
+// setting up of port
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-	console.log(`Server runnig at port ${port} !`);
+io.on('connection', () => {
+	console.log('New websocket connection added')
+})
+
+server.listen(port, () => {
+	console.log(`Server runnig at port ${port} !`)
 })
