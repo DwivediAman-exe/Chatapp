97652b90a48dcd9262eb1878efac4112f26254b5
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-30 01:46:32 +0530

    user connect and disconnet

--- src/index.js
+++ src/index.js
@@ -23,8 +23,14 @@ io.on('connection', (socket) => {
 
 	socket.emit('message', 'welcome')
 
+	socket.broadcast.emit('message', 'A new user has joined the chat :)')
+
 	socket.on('sendMessage', (message) => {
 		io.emit('message', message)
+	})
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left the chat :(')
 	})
 })
 
