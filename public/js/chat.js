98cf2952dfdd54ef45704a60c32df2bec58a21ff
Aman Dwivedi <19dwivediaman@gmail.com>
2021-04-30 11:11:04 +0530

    refractor

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,28 @@
 const socket = io()
 
+// variables
+const $messageForm = document.querySelector('#message-form') 
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
 	console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
 	e.preventDefault()
+
+	$messageFormButton.setAttribute('disabled', 'disabled')
 
 	const message = e.target.elements.message.value
 
 	socket.emit('sendMessage', message, (error) => {
+
+		$messageFormButton.removeAttribute('disabled')
+		$messageFormInput.value = ''
+		$messageFormInput.focus() 
+
 		if(error) {
 			 console.log(error)
 		}	
@@ -17,7 +30,10 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 	})
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
+
+	$locationButton.setAttribute('disabled', 'disabled')
+
 	if(!navigator.geolocation) {
 		return alert('Geolocation is not supported in your browser')
 	}
@@ -27,7 +43,9 @@ document.querySelector('#send-location').addEventListener('click', () => {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		}, () => {
+			$locationButton.removeAttribute('disabled')
 			console.log('Your location was shared !')
 		})
 	})
+
 })
