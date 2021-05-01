bc605116f1938b3726c640203929fd31f0aba826
Aman Dwivedi <19dwivediaman@gmail.com>
2021-05-01 13:07:46 +0530

    user

--- src/utils/users.js
+++ src/utils/users.js
@@ -38,13 +38,17 @@ const removeUser = (id) => {
 	}
 }
 
-addUser ({
-	id: 22,
-	username: 'aman',
-	room: 'ab'
-})
+const getUser = (id) => {
+	return users.find((user) => user.id === id )
+}
 
-const removedUser = removeUser(22)
+const getUsersInRoom = (room) => {
+	return users.filter( (user) => user.room === room )
+}
 
-console.log(removedUser)
-console.log(users)
+module.exports = {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom
+}
